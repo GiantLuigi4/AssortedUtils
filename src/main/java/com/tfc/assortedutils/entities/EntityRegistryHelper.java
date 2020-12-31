533673ce39efb9495c23b7d3999cdda47b566de2
GiantLuigi4 <tjmferet@hotmail.com>
2020-12-30 21:43:29 -0500

    ohWait

--- src/main/java/com/tfc/assortedutils/entities/EntityRegistryHelper.java
+++ src/main/java/com/tfc/assortedutils/entities/EntityRegistryHelper.java
@@ -9,9 +9,13 @@ import java.util.function.Supplier;
 
 public class EntityRegistryHelper {
 	public static <T extends Entity> Supplier<EntityType<T>> buildType(EntityType.IFactory<T> entityConstructor, ResourceLocation registryName) {
+		return buildType(entityConstructor, registryName, 1, 1);
+	}
+	
+	public static <T extends Entity> Supplier<EntityType<T>> buildType(EntityType.IFactory<T> entityConstructor, ResourceLocation registryName, float width, float height) {
 		return () -> EntityType.Builder
 				.create(entityConstructor, EntityClassification.CREATURE)
-				.setTrackingRange(64).setUpdateInterval(2).size(1.3964844f, 2.25f)
+				.setTrackingRange(64).setUpdateInterval(2).size(width, height)
 				.build(registryName.toString());
 	}
 }
