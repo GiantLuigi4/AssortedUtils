7c6c255e7cce13cb0f3c00b3c327950a18f236bc
GiantLuigi4 <tjmferet@hotmail.com>
2020-12-31 23:28:15 -0500

    moar rendering, also I fixed VoxelshapeEntity for real this time

--- src/main/java/com/tfc/assortedutils/API/rendering/RenderHelper.java
+++ src/main/java/com/tfc/assortedutils/API/rendering/RenderHelper.java
@@ -8,21 +8,57 @@ import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.math.vector.Matrix4f;
 import org.lwjgl.opengl.GL11;
 
 public class RenderHelper {
+	private static final MatrixStack blankStack = new MatrixStack();
+	
 	public static void drawBox(MatrixStack stack, AxisAlignedBB box, float r, float g, float b, float a) {
 		Tessellator tessellator = Tessellator.getInstance();
 		BufferBuilder bufferbuilder = tessellator.getBuffer();
 		bufferbuilder.begin(GL11.GL_TRIANGLE_STRIP, DefaultVertexFormats.POSITION_COLOR);
-		if (stack != null) {
-			RenderSystem.pushMatrix();
-			RenderSystem.multMatrix(stack.getLast().getMatrix());
-		}
-		WorldRenderer.addChainedFilledBoxVertices(bufferbuilder, box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ, r, g, b, a);
-		tessellator.draw();
 		if (stack != null)
-			RenderSystem.popMatrix();
+			doDrawBox(stack.getLast().getMatrix(), bufferbuilder, box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ, r, g, b, a);
+		else doDrawBox(null, bufferbuilder, box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ, r, g, b, a);
+		tessellator.draw();
+	}
+	
+	private static void doDrawBox(Matrix4f matrix4f, BufferBuilder builder, double x1, double y1, double z1, double x2, double y2, double z2, float red, float green, float blue, float alpha) {
+		if (matrix4f == null) {
+			WorldRenderer.addChainedFilledBoxVertices(builder, x1, y1, z1, x2, y2, z2, red, green, blue, alpha);
+			return;
+		}
+		builder.pos(matrix4f, (float) x1, (float) y1, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y1, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y1, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y1, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y2, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y2, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y2, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y1, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y2, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y1, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y1, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y1, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y2, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y2, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y2, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y1, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y2, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y1, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y1, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y1, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y1, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y1, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y1, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y2, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y2, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x1, (float) y2, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y2, (float) z1).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y2, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y2, (float) z2).color(red, green, blue, alpha).endVertex();
+		builder.pos(matrix4f, (float) x2, (float) y2, (float) z2).color(red, green, blue, alpha).endVertex();
 	}
 	
 	public static void drawBox(MatrixStack stack, MutableBoundingBox box, float r, float g, float b, float a) {
@@ -34,42 +70,70 @@ public class RenderHelper {
 		BufferBuilder bufferbuilder = tessellator.getBuffer();
 		bufferbuilder.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
 		RenderSystem.lineWidth(2);
-		if (stack != null) {
-			RenderSystem.pushMatrix();
-			RenderSystem.multMatrix(stack.getLast().getMatrix());
-		}
 		int x1 = box.minX;
 		int y1 = box.minY;
 		int z1 = box.minZ;
 		int x2 = box.maxX;
 		int y2 = box.maxY;
 		int z2 = box.maxZ;
-		bufferbuilder.pos(x1, y1, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y1, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y1, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y1, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y1, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y1, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y1, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y1, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y1, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y2, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y2, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y2, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y2, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y1, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y2, z1).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y2, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y2, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y1, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x2, y2, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y2, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y2, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y1, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y2, z2).color(r, g, b, a).endVertex();
-		bufferbuilder.pos(x1, y2, z1).color(r, g, b, a).endVertex();
-		tessellator.draw();
+		Matrix4f matrix4f = blankStack.getLast().getMatrix();
 		if (stack != null)
-			RenderSystem.popMatrix();
+			matrix4f = stack.getLast().getMatrix();
+		bufferbuilder.pos(matrix4f, x1, y1, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y1, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y1, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y1, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y1, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y1, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y1, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y1, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y1, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y2, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y2, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y2, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y2, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y1, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y2, z1).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y2, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y2, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y1, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x2, y2, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y2, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y2, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y1, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y2, z2).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, x1, y2, z1).color(r, g, b, a).endVertex();
+		tessellator.draw();
+	}
+	
+	public static void drawLine(MatrixStack stack,
+								double minX, double minY, double minZ,
+								double maxX, double maxY, double maxZ,
+								float r, float g, float b, float a
+	) {
+		drawLine(
+				stack,
+				minX, minY, minZ,
+				maxX, maxY, maxZ,
+				r, g, b, a, r, g, b, a
+		);
+	}
+	
+	public static void drawLine(MatrixStack stack,
+								double minX, double minY, double minZ,
+								double maxX, double maxY, double maxZ,
+								float r, float g, float b, float a,
+								float r2, float g2, float b2, float a2
+	) {
+		Matrix4f matrix4f = blankStack.getLast().getMatrix();
+		if (stack != null)
+			matrix4f = stack.getLast().getMatrix();
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferbuilder = tessellator.getBuffer();
+		bufferbuilder.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
+		RenderSystem.lineWidth(2);
+		bufferbuilder.pos(matrix4f, (float) minX, (float) minY, (float) minZ).color(r, g, b, a).endVertex();
+		bufferbuilder.pos(matrix4f, (float) maxX, (float) maxY, (float) maxZ).color(r2, g2, b2, a2).endVertex();
+		tessellator.draw();
 	}
 }
--- src/main/java/com/tfc/assortedutils/mixin_code/VoxelshapeEntity.java
+++ src/main/java/com/tfc/assortedutils/mixin_code/VoxelshapeEntity.java
@@ -11,6 +11,7 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.vector.Vector3d;
 
 public class VoxelshapeEntity {
@@ -29,28 +30,54 @@ public class VoxelshapeEntity {
 			return;
 		
 		
-		Vector3d playerPos = Minecraft.getInstance().player.getEyePosition(partialTicks);
 		double distance = 0;
-		double reach = 16;
+		double reach = 8;
 		if (mc.playerController.extendedReach())
 			distance = 6.0D;
 		
+		Vector3d playerPos = Minecraft.getInstance().player.getEyePosition(partialTicks);
 		Vector3d lookVec = entity.getLook(1.0F);
 		Vector3d reachVec = playerPos.add(lookVec.x * reach, lookVec.y * reach, lookVec.z * reach);
+		
 		AxisAlignedBB axisalignedbb = entity.getBoundingBox().expand(lookVec.scale(reach)).grow(1.0D, 1.0D, 1.0D);
 		EntityRayTraceResult result1 = ProjectileHelper.rayTraceEntities(entity, playerPos, reachVec, axisalignedbb, (p_215312_0_) -> {
 			if (p_215312_0_ == Minecraft.getInstance().player) return false;
 			boolean voxelShapeResult = true;
 			if (p_215312_0_ instanceof IVoxelShapeEntity) {
 				if (((IVoxelShapeEntity) p_215312_0_).getRaytraceShape() != null) {
-					RayTraceResult result2 = ((IVoxelShapeEntity) result.getEntity()).getRaytraceShape().rayTrace(
-							playerPos.subtract(result.getEntity().getPositionVec()), reachVec.subtract(result.getEntity().getPositionVec()), new BlockPos(0, 0, 0)
+					VoxelShape shape = ((IVoxelShapeEntity) p_215312_0_.getEntity()).getRaytraceShape();
+
+//					voxelShapeResult = false;
+//					for (AxisAlignedBB box : shape.toBoundingBoxList()) {
+////						if (box.offset(entity.getPositionVec()).rayTrace(playerPos,reachVec).isPresent()) {
+////							voxelShapeResult = true;
+////							break;
+////						}
+////						if (box.offset(entity.getPositionVec()).rayTrace(reachVec,playerPos).isPresent()) {
+////							voxelShapeResult = true;
+////							break;
+////						}
+//						if (new AxisAlignedBB(playerPos.x,playerPos.y,playerPos.z,reachVec.x,reachVec.y,reachVec.z).intersects(box))
+//							voxelShapeResult = true;
+//					}
+//					if (!voxelShapeResult) {
+					RayTraceResult result2 = shape.withOffset(
+							-(entity.getBoundingBox().maxX - entity.getBoundingBox().minX) / 2f,
+							0,
+							-(entity.getBoundingBox().maxZ - entity.getBoundingBox().minZ) / 2f
+					).withOffset(
+							-(entity.getBoundingBox().maxX - entity.getBoundingBox().minX) / 3f,
+							0,
+							-(entity.getBoundingBox().maxZ - entity.getBoundingBox().minZ) / 3f
+					).withOffset(p_215312_0_.getPosX(), p_215312_0_.getPosY(), p_215312_0_.getPosZ()).rayTrace(
+							playerPos, reachVec, new BlockPos(0, 0, 0)
 					);
 					voxelShapeResult = result2 != null;
+//					}
 				}
 			}
 			return !p_215312_0_.isSpectator() && p_215312_0_.canBeCollidedWith() && voxelShapeResult;
-		}, distance);
+		}, Integer.MAX_VALUE);
 		
 		if (result1 != null) {
 			mc.objectMouseOver = new VoxelShapeEntityRaytraceResult(result1.getEntity(), result1.getHitVec(), ((IVoxelShapeEntity) result.getEntity()).getRaytraceShape());
@@ -64,14 +91,23 @@ public class VoxelshapeEntity {
 				boolean voxelShapeResult = true;
 				if (p_215312_0_ instanceof IVoxelShapeEntity) {
 					if (((IVoxelShapeEntity) p_215312_0_).getRaytraceShape() != null) {
-						RayTraceResult result2 = ((IVoxelShapeEntity) result.getEntity()).getRaytraceShape().rayTrace(
-								playerPos.subtract(result.getEntity().getPositionVec()), reachVec.subtract(result.getEntity().getPositionVec()), new BlockPos(0, 0, 0)
+						VoxelShape shape = ((IVoxelShapeEntity) p_215312_0_).getRaytraceShape();
+						RayTraceResult result2 = shape.withOffset(
+								-(entity.getBoundingBox().maxX - entity.getBoundingBox().minX) / 2f,
+								0,
+								-(entity.getBoundingBox().maxZ - entity.getBoundingBox().minZ) / 2f
+						).withOffset(
+								-(entity.getBoundingBox().maxX - entity.getBoundingBox().minX) / 3f,
+								0,
+								-(entity.getBoundingBox().maxZ - entity.getBoundingBox().minZ) / 3f
+						).withOffset(p_215312_0_.getPosX(), p_215312_0_.getPosY(), p_215312_0_.getPosZ()).rayTrace(
+								playerPos, reachVec, new BlockPos(0, 0, 0)
 						);
 						voxelShapeResult = result2 != null;
 					}
 				}
 				return !p_215312_0_.isSpectator() && p_215312_0_.canBeCollidedWith() && voxelShapeResult;
-			}, distance);
+			}, Integer.MAX_VALUE);
 			
 			mc.objectMouseOver = entity.pick(reach, partialTicks, false);
 			
@@ -88,6 +124,14 @@ public class VoxelshapeEntity {
 					RayTraceResult result2 = ((IVoxelShapeEntity) entityraytraceresult.getEntity()).getRaytraceShape().rayTrace(
 							playerPos.subtract(entity.getPositionVec()), reachVec.subtract(entity.getPositionVec()), new BlockPos(0, 0, 0)
 					);
+					
+					if (result2 == null) {
+						Entity entity1 = entityraytraceresult.getEntity();
+						mc.objectMouseOver = entityraytraceresult;
+						if (entity1 instanceof LivingEntity || entity1 instanceof ItemFrameEntity)
+							mc.pointedEntity = entity1;
+						return;
+					}
 					
 					mc.objectMouseOver = new VoxelShapeEntityRaytraceResult(entity, result2.getHitVec(), ((IVoxelShapeEntity) entityraytraceresult.getEntity()).getRaytraceShape());
 					mc.pointedEntity = entityraytraceresult.getEntity();
