2f44279f8bb53a627c2155e4badf15275287bb67
GiantLuigi4 <tjmferet@hotmail.com>
2021-01-03 14:58:33 -0500

    Fix the fix of the fix fix fixedy fix fix fixy the fix

--- src/main/java/com/tfc/assortedutils/mixin_code/VoxelshapeEntity.java
+++ src/main/java/com/tfc/assortedutils/mixin_code/VoxelshapeEntity.java
@@ -50,9 +50,10 @@ public class VoxelshapeEntity {
 					VoxelShape shape = ((IVoxelShapeEntity) p_215312_0_.getEntity()).getRaytraceShape();
 					
 					RayTraceResult result2 = shape.withOffset(
-							-(entity.getBoundingBox().maxX - entity.getBoundingBox().minX) / 5f,
-							0,
-							-(entity.getBoundingBox().maxZ - entity.getBoundingBox().minZ) / 5f
+							-(0.25f) / 2, 0, -(0.25f) / 2
+//							-(p_215312_0_.getBoundingBox().maxX - p_215312_0_.getBoundingBox().minX) / 16f,
+//							0,
+//							-(p_215312_0_.getBoundingBox().maxZ - p_215312_0_.getBoundingBox().minZ) / 16f
 					).withOffset(p_215312_0_.getPosX(), p_215312_0_.getPosY(), p_215312_0_.getPosZ()).rayTrace(
 							reachVec, playerPos, new BlockPos(0, 0, 0)
 					);
@@ -64,9 +65,10 @@ public class VoxelshapeEntity {
 		
 		if (result1 != null) {
 			BlockRayTraceResult result2 = ((IVoxelShapeEntity) result1.getEntity()).getRaytraceShape().withOffset(
-					-(entity.getBoundingBox().maxX - entity.getBoundingBox().minX) / 5f,
-					0,
-					-(entity.getBoundingBox().maxZ - entity.getBoundingBox().minZ) / 5f
+					-(0.25f) / 2, 0, -(0.25f) / 2
+//					-(result1.getEntity().getBoundingBox().maxX - result1.getEntity().getBoundingBox().minX) / 16f,
+//					0,
+//					-(result1.getEntity().getBoundingBox().maxZ - result1.getEntity().getBoundingBox().minZ) / 16f
 			).withOffset(result1.getEntity().getPosX(), result1.getEntity().getPosY(), result1.getEntity().getPosZ()).rayTrace(
 					playerPos, reachVec, new BlockPos(0, 0, 0)
 			);
@@ -96,9 +98,10 @@ public class VoxelshapeEntity {
 					if (((IVoxelShapeEntity) p_215312_0_).getRaytraceShape() != null) {
 						VoxelShape shape = ((IVoxelShapeEntity) p_215312_0_).getRaytraceShape();
 						RayTraceResult result2 = shape.withOffset(
-								-(entity.getBoundingBox().maxX - entity.getBoundingBox().minX) / 5f,
-								0,
-								-(entity.getBoundingBox().maxZ - entity.getBoundingBox().minZ) / 5f
+								-(0.25f) / 2, 0, -(0.25f) / 2
+//								-(p_215312_0_.getBoundingBox().maxX - p_215312_0_.getBoundingBox().minX) / 16f,
+//								0,
+//								-(p_215312_0_.getBoundingBox().maxZ - p_215312_0_.getBoundingBox().minZ) / 16f
 						).withOffset(p_215312_0_.getPosX(), p_215312_0_.getPosY(), p_215312_0_.getPosZ()).rayTrace(
 								reachVec, playerPos, new BlockPos(0, 0, 0)
 						);
@@ -121,9 +124,10 @@ public class VoxelshapeEntity {
 			if (distEntity < distBlock && entityraytraceresult != null) {
 				if (entityraytraceresult.getEntity() instanceof IVoxelShapeEntity) {
 					BlockRayTraceResult result2 = ((IVoxelShapeEntity) entityraytraceresult.getEntity()).getRaytraceShape().withOffset(
-							-(entity.getBoundingBox().maxX - entity.getBoundingBox().minX) / 5f,
-							0,
-							-(entity.getBoundingBox().maxZ - entity.getBoundingBox().minZ) / 5f
+							-(0.25f) / 2, 0, -(0.25f) / 2
+//							-(entityraytraceresult.getEntity().getBoundingBox().maxX - entityraytraceresult.getEntity().getBoundingBox().minX) / 16f,
+//							0,
+//							-(entityraytraceresult.getEntity().getBoundingBox().maxZ - entityraytraceresult.getEntity().getBoundingBox().minZ) / 16f
 					).withOffset(entityraytraceresult.getEntity().getPosX(), entityraytraceresult.getEntity().getPosY(), entityraytraceresult.getEntity().getPosZ()).rayTrace(
 							playerPos, reachVec, new BlockPos(0, 0, 0)
 					);
