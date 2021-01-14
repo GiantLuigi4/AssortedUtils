331ffcdd4dab8f471516d74c4baeb8e790f03f28
GiantLuigi4 <49770992+giantluigi4@users.noreply.github.com>
2021-01-14 16:46:21 -0500

    switch to mixins for accessing NetworkRegistry$createInstance, which also fixes the issue with me not setting the method as accessible

--- src/main/java/com/tfc/assortedutils/API/networking/AutomatedSimpleChannel.java
+++ src/main/java/com/tfc/assortedutils/API/networking/AutomatedSimpleChannel.java
@@ -1,27 +1,26 @@
 package com.tfc.assortedutils.API.networking;
 
+import com.tfc.assortedutils.mixins.NetworkRegistryAccessor;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.NetworkInstance;
-import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
-import java.lang.reflect.Method;
 import java.util.function.*;
 
 public class AutomatedSimpleChannel extends SimpleChannel {
-	private static final Method createInstance;
-	
-	static {
-		try {
-			createInstance = NetworkRegistry.class.getDeclaredMethod("createInstance", ResourceLocation.class, Supplier.class, Predicate.class, Predicate.class);
-		} catch (NoSuchMethodException err) {
-			RuntimeException exception = new RuntimeException(err);
-			exception.setStackTrace(err.getStackTrace());
-			throw exception;
-		}
-	}
+//	private static final Method createInstance;
+//
+//	static {
+//		try {
+//			createInstance = NetworkRegistry.class.getDeclaredMethod("createInstance", ResourceLocation.class, Supplier.class, Predicate.class, Predicate.class);
+//		} catch (NoSuchMethodException err) {
+//			RuntimeException exception = new RuntimeException(err);
+//			exception.setStackTrace(err.getStackTrace());
+//			throw exception;
+//		}
+//	}
 	
 	private int index = 0;
 	
@@ -34,14 +33,15 @@ public class AutomatedSimpleChannel extends SimpleChannel {
 	}
 	
 	public static AutomatedSimpleChannel create(final ResourceLocation name, Supplier<String> networkProtocolVersion, Predicate<String> clientAcceptedVersions, Predicate<String> serverAcceptedVersions) {
-		try {
-			return new AutomatedSimpleChannel((NetworkInstance) createInstance.invoke(null, name, networkProtocolVersion, clientAcceptedVersions, serverAcceptedVersions));
-		} catch (Throwable err) {
-			RuntimeException exception = new RuntimeException(err);
-			//I should start doing this so the exception message is a bit more useful, lol
-			exception.setStackTrace(err.getStackTrace());
-			throw exception;
-		}
+		return new AutomatedSimpleChannel(NetworkRegistryAccessor.create(name, networkProtocolVersion, clientAcceptedVersions, serverAcceptedVersions));
+//		try {
+//			return new AutomatedSimpleChannel((NetworkInstance) createInstance.invoke(null, name, networkProtocolVersion, clientAcceptedVersions, serverAcceptedVersions));
+//		} catch (Throwable err) {
+//			RuntimeException exception = new RuntimeException(err);
+////			I should start doing this so the exception message is a bit more useful, lol
+//			exception.setStackTrace(err.getStackTrace());
+//			throw exception;
+//		}
 	}
 	
 	public <MSG extends SimplePacket> void registerPacket(Class<MSG> packetClass, Function<PacketBuffer, MSG> packetSupplier) {
